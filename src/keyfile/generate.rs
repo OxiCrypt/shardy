05804f2b4b09f6ba97fee231a0a3c550a8e99c52
Rage Fade <ragef6@tutanota.de>
2026-03-25 18:05:23 -0400

    Generate.rs was too tiny to warrant existence

--- src/keyfile/generate.rs
+++
@@ -1,7 +0,0 @@
-use rand::fill;
-use zeroize::Zeroizing;
-pub fn gen_keyfile() -> Zeroizing<[u8; 32]> {
-    let mut keybytes = Zeroizing::new([0u8; 32]);
-    fill(keybytes.as_mut());
-    keybytes
-}
--- src/keyfile/mod.rs
+++ src/keyfile/mod.rs
@@ -1,1 +1,7 @@
-mod generate;
+use rand::fill;
+use zeroize::Zeroizing;
+pub fn gen_keyfile() -> Zeroizing<[u8; 32]> {
+    let mut keybytes = Zeroizing::new([0u8; 32]);
+    fill(keybytes.as_mut());
+    keybytes
+}
